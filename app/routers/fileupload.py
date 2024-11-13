689139bfc140a8ccc0e021d133f9637d6a021a1a
Jemin Shah <Jemin.s@cctdigisol.local>
2024-11-13 19:09:05 +0530

    upload multi sheet upload and read sheet wise data

--- app/routers/fileupload.py
+++ app/routers/fileupload.py
@@ -3,10 +3,14 @@ import shutil
 import os
 import pandas as pd
 import logging
+import json
 
 router = APIRouter()
 
-ALLOWED_EXTENSIONS = {".txt"}
+ALLOWED_EXTENSIONS = {".txt",".xlsx"}
+with open('app/sheet_validate.json','r') as file:
+    sheet_data = json.load(file) 
+    
 # @router.post("/upload/")
 # async def upload_file(file:UploadFile):
 #     upload_dir = "app/files/"
@@ -54,8 +58,13 @@ async def readFile(filename:str = Query(..., min_length=1, max_length=100)):
         logging.error(f"Invalid file extension for file '{filename}'")
         raise HTTPException(status_code=400,detail="Invalid extension") 
     try:
-        with open(filepath  , "r") as f:
-            content = f.read()
+        if filename.endswith((".xlsx", ".xls")):
+            df = pd.read_excel(filepath)
+            df = df.fillna(value="")
+            content = df.to_dict(orient="records")
+        elif filename.endswith((".txt")):
+            with open(filepath  , "r") as f:
+                content = f.read()
         return {"filename": filename, "content": content}
     except FileNotFoundError:
         logging.error(f"File '{filename}' not found at path {filepath}")
@@ -85,9 +94,39 @@ async def getFile(file:bytes=File(...)):
 @router.post('/upload_option')
 async def uploadOption(upload_file:UploadFile = File(...)):
     path = f"app/files/{upload_file.filename}"
-    print(upload_file)
+    ALLOWED_SHEET_NAMES = list(sheet_data.keys())
+    print(ALLOWED_SHEET_NAMES)
     with open(path,'wb') as buffer:
         shutil.copyfileobj(upload_file.file, buffer)
+        
+    try :
+        excel_data = pd.ExcelFile(path)
+        sheet_names = excel_data.sheet_names
+        for sheet in sheet_names:
+            #sheet name validation
+            if sheet not in ALLOWED_SHEET_NAMES:
+                logging.error(f"Invalid sheet name found: {sheet}")
+                raise HTTPException(status_code=400, detail=f"Invalid sheet name: {sheet}")
+            
+            df = excel_data.parse(sheet)
+            df = df.fillna(value="")
+            actual_headers = list(df.columns)
+            
+            #header validation
+            expectedHeader = sheet_data[sheet]['headers']
+            print(expectedHeader)
+            invalid_headers = [header for header in actual_headers if header not in expectedHeader]   
+            if invalid_headers:
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Invalid headers: {invalid_headers} in {sheet}."
+                )
+          
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"An error occurred during header validation: {e}"
+        )
         
     return {  
         'file': upload_file.filename,  
@@ -95,3 +134,29 @@ async def uploadOption(upload_file:UploadFile = File(...)):
         'path': path,  
     }
     
+@router.get('/read-multisheet-file/')
+async def readMultisheetFile(filename:str = Query(..., min_length=1, max_length=100)):
+    filepath = f"app/files/{filename}"
+    ALLOWED_SHEET_NAMES = {"Sheet1", "Sheet2", "Summary"}
+    if not any(filename.endswith(ext) for ext in ALLOWED_EXTENSIONS):
+        logging.error(f"Invalid file extension for file '{filename}'")
+        raise HTTPException(status_code=400,detail="Invalid extension") 
+    try:
+        if filename.endswith((".xlsx", ".xls")):
+            excel_data = pd.ExcelFile(filepath)
+            sheet_names = excel_data.sheet_names
+            content = {}
+            for sheet in sheet_names:
+                df = excel_data.parse(sheet)
+                df = df.fillna(value="")
+                content[sheet] = df.to_dict(orient='records')
+        elif filename.endswith((".txt")):
+            with open(filepath  , "r") as f:
+                content = f.read()
+        return {"filename": filename, "content": content}
+    except FileNotFoundError:
+        logging.error(f"File '{filename}' not found at path {filepath}")
+        return {"error": "File not found"}
+    except Exception as e:
+        logging.error(f"An error occurred while reading file '{filename}': {e}")
+        return {"error":str(e)}
